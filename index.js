a34718a5f3c14762d2925723f1e564c83bbe31b5
BuildersRejected <48058454+BuildersRejected@users.noreply.github.com>
2021-01-02 15:37:39 -0600

    added config

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 const Discord = require('discord.js');
+const Config = require('Config.js');
 
 const bot = new Discord.Client();
-
-const prefix = '!';
+const config_fs = require('fs').promises;
 
 bot.once('ready', () =>{
   console.log('I like tits');
+
 });
+//  have the changing channel commands work in the channel it was sent from like: !channel Plan : which would use the current channel
+bot.on('message',async message) =>{
+  if (message.author.bot) return;
+
+  //console.log(message.content);
+  const configFile = await config_fs.readFile('./config.json','utf8');
+  const configObj = JSON.parse(configFile);
+  const configBot = configObj['bot'];
+
+  /////// prefix
+  if (configBot['customCommandPrefix'] == "true"){
+    const comm_prefix = configBot['commandPrefix'];
+  } else {
+    const comm_prefix = configBot['defaultCommandPrefix'];
+  }
+  if (configBot['useVictimRole']){
+    const victimRole = configBot['victimRole'];
+  }
+  if (configBot['useUsername']) {
+    const victimUsername = configBot['victimUsername'];
+  }
 
 bot.on('message', message =>{
     //if(!message.content.startsWith(prefix) || message.author.bot) return;
-    if(message.author.username !== "BRXathartic" || message.author.bot) return;
+    if (message.author.bot) {
+      return;
+    }
+
+    //if(message.author.username !== "BRXathartic" || message.author.bot) return;
     const command = message.content;
 
-    if (message.author.username === "BRXathartic") {
-      //message.channel.send("sup BRX?");
+    if (configBot['useUsername'] && message.author.username === victimUsername) {
       message.delete();
-      //var say = "@" + message.author.username.toString() + message.author.discriminator.toString() + " says: " + command;
       var say = message.author.toString() + " says: " + command;
-      //message.channel.send('${message.author} says: ');
+      message.channel.send(say);
+    }
+    if (configBot['useVictimRole'] && message.member.roles.find(r => r.name === victimRole){
+      message.delete();
+      var say = message.author.toString() + " says: " + command;
       message.channel.send(say);
     }
 });
+
+/*
+function planA() {
+
+}
+**/
 
 bot.login('');
